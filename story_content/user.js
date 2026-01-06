f7a734295325e3f03add74e1009d9ac195d91520
Dimas Alfayza R.E <dimas.alfaiza7@gmail.com>
2026-01-06 23:52:51 +0700

    Upload Folder

---
+++ story_content/user.js
@@ -0,0 +1,21 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6jLtCPfdWdx":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="1.mp3";
+audio.load();
+audio.play();
+audio.volume=0.4;
+
+
+}
+
